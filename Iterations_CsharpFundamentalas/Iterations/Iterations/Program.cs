fa056aaaa1f7b8a38ce30ed72a6cb4d29435b7c6
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-10-29 05:53:12 +0100

    iterations

---
+++ Iterations_CsharpFundamentalas/Iterations/Iterations/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Iterations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           /* // dipslays even number from 1 to 10
+            for (int i = 1; i < 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine("Reverse...");
+            for (int i = 10; i >= 1; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);  
+                }
+            }*/
+        }
+    }
+}
