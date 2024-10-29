90d995562f6db438efe27062ab6b168ac8a45495
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-10-29 06:00:21 +0100

    using foreach

--- Iterations_CsharpFundamentalas/Iterations/Iterations/Program.cs
+++ Iterations_CsharpFundamentalas/Iterations/Iterations/Program.cs
@@ -4,23 +4,39 @@
     {
         static void Main(string[] args)
         {
-           /* // dipslays even number from 1 to 10
-            for (int i = 1; i < 10; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine(i);
-                }
-            }
+            /* // dipslays even number from 1 to 10
+             for (int i = 1; i < 10; i++)
+             {
+                 if (i % 2 == 0)
+                 {
+                     Console.WriteLine(i);
+                 }
+             }
 
-            Console.WriteLine("Reverse...");
-            for (int i = 10; i >= 1; i--)
+             Console.WriteLine("Reverse...");
+             for (int i = 10; i >= 1; i--)
+             {
+                 if (i % 2 == 0)
+                 {
+                     Console.WriteLine(i);  
+                 }
+             }*/
+
+            // UNDERSTANDING FOREACH LOOP (comparing with for loop)
+
+            var name = "John Smith";
+            // using for loop
+            /*for (int i = 0; i < name.Length; i++)
             {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine(i);  
-                }
+                Console.WriteLine(name[i]);
             }*/
+
+            //using FOREACH LOOP
+
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
         }
     }
 }
