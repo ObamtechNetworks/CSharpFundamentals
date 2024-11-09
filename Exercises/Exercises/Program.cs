4d1f4d92c5d465f5aff3134d9769abca24cc73cb
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-11-09 13:36:58 +0100

    guessing game

--- Exercises/Exercises/Program.cs
+++ Exercises/Exercises/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Exercises
+﻿using System.Threading.Channels;
+
+namespace Exercises
 {
     internal class Program
     {
@@ -64,22 +66,80 @@
              * For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1
              * and display it as 5! = 120.
              */
-            Console.WriteLine("Please enter a number");
-            var number = Console.ReadLine();
-            try
+            /* Console.WriteLine("Please enter a number");
+             var number = Console.ReadLine();
+             try
+             {
+                 var convertedNum = Convert.ToInt32(number);
+                 var factorialResult = 1; // even if 0 is entered, loop will be false and value will be 0
+                 for (int i = convertedNum; i >= 1; --i)
+                 {
+                     factorialResult *= i;
+                 }
+                 Console.WriteLine(string.Format("{0}! = {1}", convertedNum, factorialResult));
+             }
+             catch (Exception)
+             {
+                 Console.WriteLine("Invalid input");
+             }*/
+
+            //Exercise 4
+            /**
+             * Write a program that picks a random number between 1 and 10.
+             * Give the user 4 chances to guess the number.
+             * If the user guesses the number, display “You won";
+             * otherwise, display “You lost".
+             * (To make sure the program is behaving correctly,
+             * you can display the secret number on the console first.)
+             */
+
+            // instantiate random class
+            var random = new Random();
+
+            // Generates a random number between 1 and 10
+            var randomNumber = random.Next(1, 11);
+
+            // list to store user guesses
+            var guesses = new List<int>();
+
+            Console.WriteLine("Guess the random number, between 1 and 10. You have four guesses.\nGood luck!");
+
+            for (int i = 0; i < 4; i++)
             {
-                var convertedNum = Convert.ToInt32(number);
-                var factorialResult = 1; // even if 0 is entered, loop will be false and value will be 0
-                for (int i = convertedNum; i >= 1; --i)
+                Console.Write("Enter your guess: ");
+                try
                 {
-                    factorialResult *= i;
+                    int guess = Convert.ToInt32(Console.ReadLine());
+                    guesses.Add(guess);
+
+                    // Check if the guess is correct
+                    if (guess == randomNumber)
+                    {
+                        Console.WriteLine($"You won! The secret number is {randomNumber}");
+                        break;  // Exit the loop if the user wins
+                    }
+                    else if (i < 3) // Give feedback only if it’s not the last attempt
+                    {
+                        Console.WriteLine("Incorrect! Try again...");
+                    }
                 }
-                Console.WriteLine(string.Format("{0}! = {1}", convertedNum, factorialResult));
+                catch (FormatException)
+                {
+                    Console.WriteLine("That was not a valid number. Please enter a number between 1 and 10.");
+                    i--;  // Don’t count this as an attempt if input was invalid
+                }
             }
-            catch (Exception)
+
+            // If the user didn't guess correctly after 4 attempts
+            if (!guesses.Contains(randomNumber))
             {
-                Console.WriteLine("Invalid input");
+                Console.WriteLine($"You lost! The secret number was: {randomNumber}");
             }
+
+
+
+
+        }
 }
 }
-}
+
