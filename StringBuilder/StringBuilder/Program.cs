f3bb3853d3d98f6cac3dc63a959f005ae4c1eabd
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-11-10 13:50:58 +0100

    chaining methods of stringBuilder

--- StringBuilder/StringBuilder/Program.cs
+++ StringBuilder/StringBuilder/Program.cs
@@ -9,17 +9,16 @@ namespace StringBuilder
             /*Console.WriteLine(builder);*/
             
 
-            builder.Append('-', 10);
-            builder.AppendLine();
-            builder.Append("Header");
-            builder.AppendLine();
-            builder.Append('-', 10);
-
-            builder.Replace('-', '+');
-
-            builder.Remove(0, 10);
-
-            builder.Insert(0, new string('-', 10));
+            // the append returns a stringbuilder, therefore we can chain all these methods together
+            builder.Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new string('-', 10));
+            
             Console.WriteLine(builder);
 
             Console.WriteLine("First Char: " + builder[0]); // accessing chars in builder by index
