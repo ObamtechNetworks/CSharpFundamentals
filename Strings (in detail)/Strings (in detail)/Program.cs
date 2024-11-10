1acd03db4dd5e30e9eeec7f0440b1d930b5091a4
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-11-10 12:39:42 +0100

    exploring string operations

---
+++ Strings (in detail)/Strings (in detail)/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Strings__in_detail_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "Michael Bamidele ";
+            Console.WriteLine("Trim: '{0}'", fullName.Trim());
+            Console.WriteLine("ToUpper: '{0}'", fullName.Trim().ToUpper());
+
+            var index = fullName.IndexOf((' '));
+            var firstName = fullName.Substring(0, index);
+            var lastName = fullName.Substring(index + 1);
+            Console.WriteLine("FirstName: " + firstName);
+            Console.WriteLine("LastName: " + lastName);
+
+            // achieve same thing using split
+            var names = fullName.Split(' ');
+            Console.WriteLine("FirstName: " + names[0]);
+            Console.WriteLine("LastName: " + names[1]);
+
+            // Using Replace
+            Console.WriteLine(fullName.Replace("Michael", "Mike"));
+
+            // dealing with null values => isNull or whitespace, isnullorempty
+            if (string.IsNullOrEmpty(" ".Trim()))
+                Console.WriteLine("Invalid");
+
+            // converting string to numbers
+            var str = "25";
+            var age = Convert.ToByte(str); // only one byte required to store someone's age
+            Console.WriteLine(age);
+
+            float price = 29.95f;
+            Console.WriteLine(price.ToString("C0")); // convert to currency
+
+
+
+        }
+    }
+}
--- Working with Dates/Working with Dates/Program.cs
+++ Working with Dates/Working with Dates/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             // working with datetime in c#
-            var dateTime = new DateTime(2015, 1, 1);
+            /*var dateTime = new DateTime(2015, 1, 1);
             var now = DateTime.Now;
             var today = DateTime.Today;
 
@@ -24,7 +24,37 @@
             // dispaly both date and time
             Console.WriteLine(now.ToString());
             // specify format
-            Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm"));
+            Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm"));*/
+
+            // Time span
+            // Time span represents a length of time
+            var timeSpan = new TimeSpan(1, 2, 3); // h m secs
+            var timeSpan1 = new TimeSpan(1, 0, 0);
+            var timeSpan2 = TimeSpan.FromHours(1); /// using static methods
+
+            // getting timespan from date arithmetics
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+
+            var duration = end - start;
+            Console.WriteLine("Duration: " + duration);
+
+            // Properties
+            Console.WriteLine("Minutes: " + timeSpan.Minutes);
+            Console.WriteLine("Total Minutes: " + timeSpan.TotalMinutes);
+
+            // timespan also is immutable
+            // methods
+
+            // Add
+            Console.WriteLine("Add Example: " + timeSpan.Add(TimeSpan.FromMinutes(8))); // becomes 1, 10, 3
+            Console.WriteLine("Subtract Example: " + timeSpan.Subtract(TimeSpan.FromMinutes(2)));
+
+            // converting timespan to string using ToString methd
+            Console.WriteLine("ToString " + timeSpan.ToString());
+
+            // Parse
+            Console.WriteLine("Parse: " + TimeSpan.Parse("01:02:03"));
         }
     }
 }
