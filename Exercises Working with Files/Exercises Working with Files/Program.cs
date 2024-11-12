f43a3801cff46a007363de01fd0610c4491908ec
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-11-12 22:56:46 +0100

    reading text in file

---
+++ Exercises Working with Files/Exercises Working with Files/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercises_Working_with_Files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /**
+             * 1- Write a program that reads a text file and displays the number of words.
+             */
+
+            // use the File Class to read text file and then display word count
+            var path = @"C:\Users\mbamidele\Documents\file.txt.txt";
+
+            var texts = File.ReadAllText(path);
+
+            var words = texts.Split(' ');
+
+            Console.WriteLine("Total words: " + words.Length);
+        }
+    }
+}
