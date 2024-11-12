66f08bd94e71c15fc66664d59948120469fe32c1
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-11-12 01:20:48 +0100

    Directory and DirectoryInfo

--- Working with Files/Working with Files/Program.cs
+++ Working with Files/Working with Files/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            var path = @"c:\somefile.jpg";
+            /*var path = @"c:\somefile.jpg";
             // Demo File and FileInfo
             File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
 
@@ -29,8 +29,32 @@
             }
 
             // file provides static method (effiicent for small file operation),
-            // fileinfo provides instance methods (effiicent for large file operations)
+            // fileinfo provides instance methods (effiicent for large file operations)*/
 
+
+
+            // WORKING WITH DIRECTORIES
+            // Similar to file and fileInfo, Directory and DirectoryInfo provide static and instance methods
+
+            Directory.CreateDirectory(@"c:\temp\folder1");
+
+            // listing all files in a given directory
+            /*var files = Directory.GetFiles(@"c:\Users\mbamidele\Projects\CSharpFundamentals", "*.sln*", SearchOption.AllDirectories);
+
+            foreach (var file in files)
+                Console.WriteLine(file);*/
+
+            // listing all directories in a given path
+            var directories = Directory.GetDirectories(@"c:\Users\mbamidele\Projects\CSharpFundamentals",
+                "*.*", searchOption: SearchOption.AllDirectories);
+
+            foreach (var directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+
+            // check if a directory exists
+            Directory.Exists("...");
 
         }
     }
