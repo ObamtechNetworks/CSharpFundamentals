f2b66ef74893e107a9a17375919fbc17fabebbae
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-11-12 01:32:57 +0100

    working with directories Directory and DirectoryInfo

--- Working with Files/Working with Files/Program.cs
+++ Working with Files/Working with Files/Program.cs
@@ -36,13 +36,13 @@
             // WORKING WITH DIRECTORIES
             // Similar to file and fileInfo, Directory and DirectoryInfo provide static and instance methods
 
-            Directory.CreateDirectory(@"c:\temp\folder1");
+            /*Directory.CreateDirectory(@"c:\temp\folder1");
 
             // listing all files in a given directory
             /*var files = Directory.GetFiles(@"c:\Users\mbamidele\Projects\CSharpFundamentals", "*.sln*", SearchOption.AllDirectories);
 
             foreach (var file in files)
-                Console.WriteLine(file);*/
+                Console.WriteLine(file);#1#
 
             // listing all directories in a given path
             var directories = Directory.GetDirectories(@"c:\Users\mbamidele\Projects\CSharpFundamentals",
@@ -55,6 +55,36 @@
 
             // check if a directory exists
             Directory.Exists("...");
+
+            
+
+            // Working with DirectoryInfo
+            var directoryInfo = new DirectoryInfo("...");
+            // start performing operation on the directory instance created
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();*/
+
+
+            // WORKING WITH THE Path Class
+
+            var path = @"C:\mbamidele\Projects\CSharpFundamentals\HelloWorld\HelloWorld.sln";
+
+            // doing lowlevel operation without using the Path class
+            var dotIndex = path.IndexOf('.');
+            var extension = path.Substring(dotIndex);
+
+            // DOING THIS WITH THE Path Class
+            Console.WriteLine("Extension: " + Path.GetExtension(path));
+
+            // extract only file name in path
+            Console.WriteLine("File Name: " + Path.GetFileName(path));
+            
+            // get file name without extension
+            Console.WriteLine("File Name without Extension: " + Path.GetFileNameWithoutExtension(path));
+
+            // Get directory name of path
+            Console.WriteLine("Directory Name of path: " + Path.GetDirectoryName(path));
+
 
         }
     }
