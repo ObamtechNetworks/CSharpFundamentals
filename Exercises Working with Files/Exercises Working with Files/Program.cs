8670cb41eae4b977a738dde39f61f7df49e5a34c
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-11-12 23:09:55 +0100

    Reading file and performing operations

--- Exercises Working with Files/Exercises Working with Files/Program.cs
+++ Exercises Working with Files/Exercises Working with Files/Program.cs
@@ -9,13 +9,35 @@
              */
 
             // use the File Class to read text file and then display word count
-            var path = @"C:\Users\mbamidele\Documents\file.txt.txt";
+            /*var path = @"C:\Users\mbamidele\Documents\file.txt.txt";
 
             var texts = File.ReadAllText(path);
 
             var words = texts.Split(' ');
 
-            Console.WriteLine("Total words: " + words.Length);
+            Console.WriteLine("Total words: " + words.Length);*/
+
+            /**
+             * 2- Write a program that reads a text file
+             * and displays the longest word in the file.
+             */
+
+            // use the File Class to read text file and then display word count
+            var path = @"C:\Users\mbamidele\Documents\file.txt.txt";
+
+            var texts = File.ReadAllText(path);
+
+            var words = texts.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            var longestWord = "";
+            // determine the longest word in the word list
+                foreach (var word in words)
+            {
+                if (word.Length > longestWord.Length)
+                    longestWord = word;
+            }
+
+            Console.WriteLine("Longest word: " + longestWord);
         }
     }
 }
